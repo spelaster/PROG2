2ca5e91eca1dd764378413f7c2691f2c1bf9c86e
nastjabesednjak <nastja.besednjak@student.fmf.uni-lj.si>
2020-04-07 22:25:58 +0200

    Update ProjektKnjige.py

--- ProjektKnjige.py
+++ ProjektKnjige.py
@@ -19,27 +19,36 @@ def zapisi_json(objekt, ime_datoteke):
         json.dump(objekt, json_datoteka, indent=4, ensure_ascii=False)
 
 vzorec = re.compile(
-    r'<a title="(?P<naslov>[^"|(]*[^ |"|(]).*?'
-    r'<span itemprop="name">(?P<avtor>.+?)</span></a>.*?'
-    r'avg rating (?P<ocena>\d{1}\.\d{2}) —.*?'
-    r'published (?P<leto>\d{4}).*?</span>',
+    #naslov
+    r"aria-level='4'>(?P<naslov>[^<|(]*[^ |(|<]).*?"
+    #avtor
+    r'<span itemprop="name">(?P<avtor>.*?)</span>.*?'
+    #povprečna ocena
+    r' (?P<ocena>\d{1}\.\d{2}) avg rating.*?'
+    #ocena na podlagi števila glasovalcev in njihovih ocen
+    r'score: (?P<score>.*?)</a>.*?'
+    #stevilo glasovalcev
+    r'>(?P<glasovalci>[\d|,]*) people voted.*?',
     re.DOTALL
 )
 
 def cisti_podatki(podatki):
     podatki_knjige = podatki.groupdict()
-    podatki_knjige['leto'] = int(podatki_knjige['leto'])
-    podatki_knjige['ocena'] = float(podatki_knjige['ocena'].replace(',', '.'))
+    #podatki_knjige['ocena'] = float(podatki_knjige['ocena'].replace(',', '.'))
+    #podatki_knjige['score'] = float(podatki_knjige['score'].replace(',', '.'))
+    #podatki_knjige['glasovalci'] = int(podatki_knjige['glasovalci'])
     return podatki_knjige
 
 for i in range(1, 11):
-    url = ('https://www.goodreads.com/shelf/show/21st-century?page={}').format(i)
+    url = ('https://www.goodreads.com/list/show/7.Best_Books_of_the_21st_Century?page={}').format(i)
+    #url = ('https://www.goodreads.com/shelf/show/21st-century?page={}').format(i)
     orodja.shrani_spletno_stran(url, 'stran-{}.html'.format(i))
 
 podatki = []
 vsebina = orodja.vsebina_datoteke('knjige21.html')
 for ujemanje in vzorec.finditer(vsebina):
     podatki_knjige = cisti_podatki(ujemanje)
+    print(podatki_knjige)
     podatki.append(podatki_knjige)
-zapisi_csv(podatki, ['naslov', 'avtor', 'leto', 'ocena'], 'knjige21.csv')
+zapisi_csv(podatki, ['naslov', 'avtor', 'ocena', 'score', 'glasovalci'], 'knjige21.csv')
 zapisi_json(podatki, 'filmi.json')
